af0a1fe2d404f9e23fb2224a5a8a55e9a638b526
JT W <jwolohan@gmail.com>
2018-05-14 19:15:53 -0400

    Added pairs maker

--- src/philosophy_generator/core.clj
+++ src/philosophy_generator/core.clj
@@ -1,6 +1,8 @@
 (ns philosophy-generator.core
   (:gen-class)
-  (:require [clojure.string :as str])
+  (:require 
+    [clojure.core.reducers :as reducers]
+    [clojure.string :as str])
 )
 
 (defn clean-and-tokenize
@@ -14,6 +16,31 @@
       (str/replace text #"([A-Z ]+ BOOK)|([IVX]+.\s)" "")
       #"\s+" " ")
   #"[.!?;]"))
+)
+
+(defn token-target
+  "Turn tokens into pair"
+  [tkns]
+  (let [n (count tkns)]
+  (str (str/join ", "  (take (- n 1) tkns)) "=>" (last tkns)))
+)
+
+(defn sent-token-target
+  "Turn sentence into token-target pairs"
+  [sentence]
+  (loop [pairs [] snt sentence]
+  (if (zero? (count snt)) pairs
+  (let [new-pairs (conj pairs 
+    (token-target (take 2 snt))
+    (token-target (take 3 snt))
+    (token-target (take 4 snt)))]
+    (println pairs)
+    (recur new-pairs (rest snt))))))
+
+(defn tokens-to-probs
+  "Turn a sequence of tokenized sentences into a count map"
+  [sentences]
+  (identity sentences)
 )
 
 (defn -main
