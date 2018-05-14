f4252cf8bfc732117d93aecbc177e13b4c0493cc
JT W <jwolohan@gmail.com>
2018-05-14 13:56:54 -0400

    Clean and tokenize function added

--- src/philosophy_generator/core.clj
+++ src/philosophy_generator/core.clj
@@ -1,5 +1,20 @@
 (ns philosophy-generator.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str])
+)
+
+(defn clean-and-tokenize
+  "Clean remove 'THIRD BOOK' and 'IV.' from text"
+  [text]
+  (map #(concat
+          (concat ["^S" "^S" "^S"] (str/split % #"\s"))
+        ["^E" "^E" "^E"])
+  (str/split
+    (str/replace
+      (str/replace text #"([A-Z ]+ BOOK)|([IVX]+.\s)" "")
+      #"\s+" " ")
+  #"[.!?;]"))
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
